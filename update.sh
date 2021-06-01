da550a522601ec92fd8dd1b4edc04928efe84f94
Melissa <spansjh@gmail.com>
2021-06-01 15:03:14 +0200

    Push update script

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ curl -s https://uat.trackmania-next-dev.core.nadeo.online/api/routes | python3 -
 curl -s https://lt.trackmania-next-dev.core.nadeo.online/api/routes | python3 -m json.tool > lt.json
 git add *.json
 git commit -m "$(date)"
-#git push origin master
+git push origin master
