549e4cb0baa81c197f67e1d70f071d609ad7ed7b
Melissa <spansjh@gmail.com>
2021-06-01 15:02:55 +0200

    Fixed update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -s https://prod.trackmania.core.nadeo.online/api/routes | python3 -m json.tool > prod.json
+curl -s https://uat.trackmania-next-dev.core.nadeo.online/api/routes | python3 -m json.tool > uat.json
+curl -s https://lt.trackmania-next-dev.core.nadeo.online/api/routes | python3 -m json.tool > lt.json
+git add *.json
+git commit -m "$(date)"
+#git push origin master
