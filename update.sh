4ef2657412668281e7be5518092ba99a578990f5
Melissa <spansjh@gmail.com>
2021-06-08 16:21:51 +0200

    Updated script to not push empty files

--- update.sh
+++ update.sh
@@ -2,6 +2,18 @@
 curl -s https://prod.trackmania.core.nadeo.online/api/routes | python3 -m json.tool > prod.json
 curl -s https://uat.trackmania-next-dev.core.nadeo.online/api/routes | python3 -m json.tool > uat.json
 curl -s https://lt.trackmania-next-dev.core.nadeo.online/api/routes | python3 -m json.tool > lt.json
-git add *.json
+
+if [ -s prod.json ]; then
+	git add prod.json
+fi
+
+if [ -s uat.json ]; then
+	git add uat.json
+fi
+
+if [ -s lt.json ]; then
+	git add lt.json
+fi
+
 git commit -m "$(date)"
 git push origin master
